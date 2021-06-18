55774c3ab9b9dd8c1ef5fe3f029b9d6c6e17a5f5
Filipp Gostev <fgostev@Filipps-MacBook-Air.local>
2021-06-17 20:26:02 -0500

    first functionality, finished 1st styling

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const modal = document.getElementById('modalForm');
+const openModalBtn = document.getElementById('new');
+const closeModalBtn = document.getElementById('closeBtn');
+
+const form = document.querySelector("form");
+const done = document.getElementById('done');
+
+// library object
+
 let bookLibrary = [];
 
 function Book(){
@@ -7,15 +16,55 @@ function Book(){
     this.read = read
 };
 
+// check the radioboxes
+// check the required
+
 
 function addBookToLibrary(){
     const newBook = Object.create(Book)
-    newBook.title = prompt('title')
-    newBook.author = prompt('author')
-    newBook.pages = prompt('pages')
-    newBook.red = prompt('read')
+    newBook.title = document.getElementById('title').value;
+    newBook.author = document.getElementById('author').value;
+    newBook.pages = document.getElementById('pages').value;
+    
+    if(document.getElementById('yes').checked == true){
+        newBook.read = "no";
+    }else if ((document.getElementById('no').checked == true)){ 
+        newBook.read = "yes";
+    }
+    
     bookLibrary.push(newBook);
+    event.preventDefault();
+    modal.style.display = 'none';
+    form.reset();
 }
 
 console.log(bookLibrary);
 
+// function for modal
+
+    function openModal(){
+        modal.style.display = 'block';
+    }
+
+    function closeModal(){
+        modal.style.display = 'none';
+    }
+    function clickOutside(e){
+        if(e.target == modal){
+            modal.style.display = 'none';
+        }
+    }
+
+    function testResults (form) {
+        var TestVar = form.inputbox.value;
+        alert ("You typed: " + TestVar);
+    }
+
+
+done.addEventListener("click", addBookToLibrary);
+
+
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside);
